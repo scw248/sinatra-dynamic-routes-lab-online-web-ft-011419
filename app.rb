002cd33aaebf78756c03c7dab6551ff97b091553
Stephen Wright <scw248@gmail.com>
2019-02-25 21:44:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   
   get "/reversename/:name" do    
     @user_name = params[:name]    
-    "Hello #{@user_name}!" 
+    "Hello #{@user_name.reverse}!" 
   end
     
   get "/square/:number" do    
