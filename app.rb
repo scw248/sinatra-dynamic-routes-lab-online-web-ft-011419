0ec45b5d7a673f863ce3e74a899e2f441b34dd3f
Stephen Wright <scw248@gmail.com>
2019-02-25 21:48:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,9 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do    
     @user_number = params[:number]    
     @user_phrase = params[:phrase] 
-    while @user_number > 0 do
+    @user_number.times do
+      "#{@user_phrase}"
+    end
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do    
