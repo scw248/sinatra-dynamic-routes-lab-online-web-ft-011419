76b0fe536cd1e74b8dccb320fe929fae995d3939
Stephen Wright <scw248@gmail.com>
2019-02-25 22:12:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
   
-  get'/:operation/:number1/:number2' do    
+  get '/:operation/:number1/:number2' do    
     @user_operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
