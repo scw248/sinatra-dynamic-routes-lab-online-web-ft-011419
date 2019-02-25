c68e7975c4a6ef3b1f6bb554c44482591de58a88
Stephen Wright <scw248@gmail.com>
2019-02-25 21:43:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,29 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get "/reversename/:name" do    
+    @user_name = params[:name]    
+    "Hello #{@user_name}!" 
+  end
+    
+  get "/square/:number" do    
+    @user_name = params[:name]    
+    "Hello #{@user_name}!" 
+  end
   
+  get "/say/:number/:phrase" do    
+    @user_name = params[:name]    
+    "Hello #{@user_name}!" 
+  end
+  
+  get "/hello/:name" do    
+    @user_name = params[:name]    
+    "Hello #{@user_name}!"
+  end
+  
+  get "/hello/:name" do    
+    @user_name = params[:name]    
+    "Hello #{@user_name}!"  
+  end
 
 end
